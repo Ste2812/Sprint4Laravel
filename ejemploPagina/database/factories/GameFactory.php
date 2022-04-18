919b79aaa82083ad2a200a7205e138d9affb2ea9
Ste2812 <stefanogallone@hotmail.com>
2022-04-18 14:08:42 +0200

    actualización migraciones y factory añadiendo campo 'slug' para crear rutas amigables

--- ejemploPagina/database/factories/GameFactory.php
+++ ejemploPagina/database/factories/GameFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Game;
 use App\Models\Team;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Game>
@@ -19,8 +20,10 @@ class GameFactory extends Factory
     protected $model = Game::class;
     public function definition()
     {
+        $name=$this->faker->sentence();
         return [
             'fecha'=>$this->faker->date(),
+            'slug'=>Str::slug($name, '_'),
             'lugar'=>$this->faker->city(),
             'id_equipo_A'=>$this->faker->randomElement(Team::all()->pluck('id')->toArray()),
             'id_equipo_B'=>$this->faker->randomElement(Team::all()->pluck('id')->toArray()),
